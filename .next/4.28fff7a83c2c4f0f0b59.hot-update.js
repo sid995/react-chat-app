13824cea83a7e68a1e394e8b61703b3a53a65096
Siddharth Kundu <sk31395@gmail.com>
2018-06-27 18:50:18 +0530

    Chat App Using React

---
+++ .next/4.28fff7a83c2c4f0f0b59.hot-update.js
@@ -0,0 +1,221 @@
+webpackHotUpdate(4,{
+
+/***/ "./pages/index.js":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* WEBPACK VAR INJECTION */(function(module) {/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react__ = __webpack_require__("./node_modules/react/cjs/react.development.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_react___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_react__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__components_Layout__ = __webpack_require__("./components/Layout.js");
+var _jsxFileName = "C:\\Users\\siddharth.kundu\\Desktop\\repositories\\react-chat-app\\pages\\index.js";
+
+(function () {
+  var enterModule = __webpack_require__("./node_modules/react-hot-loader/index.js").enterModule;
+
+  enterModule && enterModule(module);
+})();
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+
+
+
+var IndexPage =
+/*#__PURE__*/
+function (_Component) {
+  _inherits(IndexPage, _Component);
+
+  function IndexPage() {
+    var _ref;
+
+    var _temp, _this;
+
+    _classCallCheck(this, IndexPage);
+
+    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+      args[_key] = arguments[_key];
+    }
+
+    return _possibleConstructorReturn(_this, (_temp = _this = _possibleConstructorReturn(this, (_ref = IndexPage.__proto__ || Object.getPrototypeOf(IndexPage)).call.apply(_ref, [this].concat(args))), Object.defineProperty(_assertThisInitialized(_this), "state", {
+      configurable: true,
+      enumerable: true,
+      writable: true,
+      value: {
+        user: null
+      }
+    }), Object.defineProperty(_assertThisInitialized(_this), "handleKeyUp", {
+      configurable: true,
+      enumerable: true,
+      writable: true,
+      value: function value(evt) {
+        if (evt.keyCode == 13) {
+          var user = evt.target.value;
+
+          _this.section({
+            user: user
+          });
+        }
+      }
+    }), _temp));
+  }
+
+  _createClass(IndexPage, [{
+    key: "render",
+    value: function render() {
+      var user = this.state.user;
+      var nameInputStyles = {
+        background: 'transparent',
+        color: '#999',
+        border: 0,
+        borderBottom: '1px solid #666',
+        borderRadius: 0,
+        fontSize: '3rem',
+        fontWeight: 500,
+        boxShadow: 'none !important'
+      };
+      return __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(__WEBPACK_IMPORTED_MODULE_1__components_Layout__["a" /* default */], {
+        pageTitle: "React Chat App",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 27
+        }
+      }, __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("main", {
+        className: "container-fluid position-absolute h-100 bg-dark",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 28
+        }
+      }, __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("div", {
+        className: "row position-absolute w-100 h-100",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 29
+        }
+      }, __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("section", {
+        className: "col-md-8 d-flex flex-row flex-wrap align-items-center align-content-center px-5",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 30
+        }
+      }, __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("div", {
+        className: "px-5 mx-5",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 31
+        }
+      }, __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("span", {
+        className: "d-block w-100 h1 text-light",
+        style: {
+          marginTop: -50
+        },
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 32
+        }
+      }, user ? __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("span", {
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 37
+        }
+      }, __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("span", {
+        style: {
+          color: '#999'
+        },
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 38
+        }
+      }, "Hello!"), user) : "Whats your name?"), !user && __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("input", {
+        type: "text",
+        className: "form-control mt-3 px-3 py-2",
+        onKeyUp: this.handleKeyUp,
+        autoComplete: "off",
+        style: nameInputStyles,
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 48
+        }
+      }))), __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement("section", {
+        className: "col-md-4 position-relative d-flex flex-wrap h-100 align-items-start align-content-between bg-white px-0",
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 52
+        }
+      }))));
+    }
+  }, {
+    key: "__reactstandin__regenerateByEval",
+    // @ts-ignore
+    value: function __reactstandin__regenerateByEval(key, code) {
+      // @ts-ignore
+      this[key] = eval(code);
+    }
+  }]);
+
+  return IndexPage;
+}(__WEBPACK_IMPORTED_MODULE_0_react__["Component"]);
+
+var _default = function _default() {
+  return __WEBPACK_IMPORTED_MODULE_0_react___default.a.createElement(IndexPage, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 61
+    }
+  });
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (_default);
+;
+
+(function () {
+  var reactHotLoader = __webpack_require__("./node_modules/react-hot-loader/index.js").default;
+
+  var leaveModule = __webpack_require__("./node_modules/react-hot-loader/index.js").leaveModule;
+
+  if (!reactHotLoader) {
+    return;
+  }
+
+  reactHotLoader.register(IndexPage, "IndexPage", "C:\\Users\\siddharth.kundu\\Desktop\\repositories\\react-chat-app\\pages\\index.js");
+  reactHotLoader.register(_default, "default", "C:\\Users\\siddharth.kundu\\Desktop\\repositories\\react-chat-app\\pages\\index.js");
+  leaveModule(module);
+})();
+
+;
+    (function (Component, route) {
+      if(!Component) return
+      if (false) return
+      module.hot.accept()
+      Component.__route = route
+
+      if (module.hot.status() === 'idle') return
+
+      var components = next.router.components
+      for (var r in components) {
+        if (!components.hasOwnProperty(r)) continue
+
+        if (components[r].Component.__route === route) {
+          next.router.update(r, Component)
+        }
+      }
+    })(typeof __webpack_exports__ !== 'undefined' ? __webpack_exports__.default : (module.exports.default || module.exports), "/")
+  
+/* WEBPACK VAR INJECTION */}.call(__webpack_exports__, __webpack_require__("./node_modules/webpack/buildin/harmony-module.js")(module)))
+
+/***/ })
+
+})
+//# sourceMappingURL=4.28fff7a83c2c4f0f0b59.hot-update.js.map
